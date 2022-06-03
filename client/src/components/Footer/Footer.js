25ead03260e8d849cee26b85e9bf9cd72d7a5b0e
Eslam-Nasr86 <eslamnasr2018@gmail.com>
2022-06-03 20:49:24 +0200

    add data json and words js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../../css/Footer/Footer.css"
+import {words} from '../../words'
+
 
 export default function Footer() {
   return (
     <footer>
 
-        &copy; copyright Eslam
+        &copy; {words.footerTitle}
     </footer>
   )
 }
---
+++ client/src/words.js
@@ -0,0 +1,7 @@
+export const words= {
+
+    headerTitle: 'Shoping cart',
+    footerTitle: 'copyright Eslam',
+    content: 'Content'
+
+}
