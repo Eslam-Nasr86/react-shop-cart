e8641a3966340b9d7f9c6117575e59c1443b77c3
Eslam-Nasr86 <eslamnasr2018@gmail.com>
2022-06-03 19:16:12 +0200

    start react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="layout">
+        <header>Shopping Cart</header>
+        <main>Content</main>
+        <footer>Footer</footer>
+
+    </div>
+  );
+}
+
+export default App;
